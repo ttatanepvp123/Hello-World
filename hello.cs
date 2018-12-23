36b640491f84a626cd49bfb59a6cc1afcd71390e
Ttatanepvp123 <ttatane974@gmail.com>
2018-12-23 13:47:28 +0100

    Hello World C#

---
+++ hello.cs
@@ -0,0 +1,18 @@
+//Thanks Salium_
+using System;
+using System.Threading;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World 1\n");
+            Console.WriteLine("Hello World 2");
+
+            Thread.Sleep(-1);
+        }
+    }
+}
